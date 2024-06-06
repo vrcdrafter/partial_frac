8ae334f122208cc191eb34169ed6cd00c2706245
Mat Hawley <mathewjonhawley@gmail.com>
2024-06-06 11:14:10 -0400

    started new routine

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 def main():
     debug = False
     if debug == True:
@@ -23,17 +22,19 @@ def main():
             # find inter values
             inter_values = []
             for i in range(len(bottom)):
-
                 inter_values.append(extract_zero_value(bottom[i]))
 
-            print(solver(top, bottom, inter_values[0])) # works but you cant call is sucessivly
+            print(solver(top, bottom, inter_values[0]))  # works but you cant call is sucessivly
             print(solver(top, bottom, inter_values[1]))
             print(solver(top, bottom, inter_values[2]))
 
 
 # auxillary stuff
 
-def split_terms(term): # this breaks (x)(x+2) into terms x and X+2
+def split_terms(term):  # this breaks (x)(x+2) into terms x and X+2, or if its a power term the last number is the power
+    if "^" in term:
+        term = term.replace(")^", "(")
+        print("term",term)
     arr = term.split('(')
     for i in range(len(arr)):
         arr[i] = arr[i].strip(')')
@@ -41,26 +42,28 @@ def split_terms(term): # this breaks (x)(x+2) into terms x and X+2
             print("found emtpy value ")
     arr.remove('')
     return arr
+
+
 def find_inter_value(arg):
     # break into parts in order
     chars = [char for char in arg]
     for i in range(len(chars)):
         try:
-            if (chars[i].isdigit() and chars[i+1].isdigit()):
-                print("found two sequential digits", chars[i],chars[i+1] )
-                temp=str(chars[i+1])+str(chars[i])
-                chars[i+1] = temp
+            if (chars[i].isdigit() and chars[i + 1].isdigit()):
+                print("found two sequential digits", chars[i], chars[i + 1])
+                temp = str(chars[i + 1]) + str(chars[i])
+                chars[i + 1] = temp
                 chars.remove(chars[i])
 
         except:
             print("move on")
     return chars
-def extract_zero_value(values):
 
-    sign = 0 # one being positive
+
+def extract_zero_value(values):
+    sign = 0  # one being positive
     for i in range(len(values)):
         if values[i] == '+':
-
             sign = 1
         if values[i].isdigit():
 
@@ -74,27 +77,30 @@ def extract_zero_value(values):
     if len(values) == 1:
         return 0
 
-def solver(top,bottom,current_inter_value): # the solver takes all denom and numerator ,
+
+def solver(top, bottom, current_inter_value):  # the solver takes all denom and numerator ,
     top_s = []
     bottom_s = []
-    top_s = top[:] #  creates a barrier for the lists so the origional does not get modified .
-    bottom_s = bottom[:] # so this is bizzare but it seems to keep the list local, you need the ":"
+    top_s = top[:]  # creates a barrier for the lists so the original does not get modified .
+    bottom_s = bottom[:]  # so this is bizzare, but it seems to keep the list local, you need the ":"
 
     for i in range(len(bottom_s)):
 
         try:
-            top_s[i] = top_s[i].replace("s",str(current_inter_value))
+            top_s[i] = top_s[i].replace("s", str(current_inter_value))
         except:
             pass
-        bottom_s[i] = bottom_s[i].replace("s",str(current_inter_value))
-        #toss out the zero entry
+        bottom_s[i] = bottom_s[i].replace("s", str(current_inter_value))
+        # toss out the zero entry
         if eval(bottom_s[i]) == 0:
             bottom_s[i] = 1
         else:
             bottom_s[i] = eval(bottom_s[i])
     for i in range(len(bottom_s)):
         try:
-            bottom_s[0] = bottom_s[0] * bottom_s[i+1] # this is a problem
+            bottom_s[0] = bottom_s[0] * bottom_s[i + 1]  # this is a problem
         except:
-            return str(eval(top_s[0]))+"/"+str(bottom_s[0])
+            return str(eval(top_s[0])) + "/" + str(bottom_s[0])
+
+
 main()
